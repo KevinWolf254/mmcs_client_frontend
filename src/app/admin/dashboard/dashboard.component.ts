ac00b25c1ef15154fd6c79dd4a9309c7a732668a
Kanyi-PC <mbugua.kevinkanyi@gmail.com>
2018-07-05 18:26:36 +0300

    c

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UnitsComponent } from '../units/units.component';
 import { CampaignService } from '../../shared/services/campaign/campaign.service';
 import { MonthlyExpenditure } from '../../shared/models/monthly-expenditure.model';
+import { UnitsService } from '../../shared/services/units/units.service';
 
 @Component({
   selector: 'app-dashboard',
