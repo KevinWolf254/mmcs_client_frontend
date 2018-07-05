f0ed0edc3fa11297589ae81d481ec2fd38c5781c
Kanyi-PC <mbugua.kevinkanyi@gmail.com>
2018-07-05 18:36:32 +0300

    commit

--- src/app/shared/models/employer.model.ts
+++ src/app/shared/models/employer.model.ts
@@ -1,10 +1,18 @@
 export class Employer {
     id: number;
     name: string;
+    constructor(id?: number, name?: string){
+        this.id = id;
+        this.name = name;
+    }
 }
 
 export class UnitsAvailableRequest extends Employer{
     adminEmail: string;
+    constructor(id?: number, name?: string, adminEmail?: string,){
+        super(id, name);
+        this.adminEmail = adminEmail;
+    }
 }
 
 export class UnitsAvailableResponse extends UnitsAvailableRequest{
@@ -17,10 +25,8 @@ export class UnitsRequest extends UnitsAvailableRequest{
     unitsRequested: number;
     mpesaTransNo: string;
 
-    constructor(id: number, name: string, adminEmail: string, unitsRequested: number, mpesaTransNo: string){
-        super();
-        this.id = id;
-        this.adminEmail = adminEmail;
+    constructor(id?: number, name?: string, adminEmail?: string, unitsRequested?: number, mpesaTransNo?: string){
+        super(id, name, adminEmail);
         this.name = name;
         this.unitsRequested = unitsRequested;
         this.mpesaTransNo = mpesaTransNo;
