9fc6a990d4e84e8d85a87a904943034f5d092f46
Kanyi-PC <mbugua.kevinkanyi@gmail.com>
2018-07-31 15:39:43 +0300

    commi

--- src/app/admin/users/user/user.component.ts
+++ src/app/admin/users/user/user.component.ts
@@ -3,7 +3,6 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { selectValidator } from '../../../shared/validators/select-validator';
 import { User } from '../../../shared/models/user.model';
 import { UserService } from '../../../shared/services/user/user.service';
-import { UserDetails } from '../../../shared/models/user-details.model';
 import { ToastrService } from 'ngx-toastr';
 import { Role } from '../../../shared/models/credentials.model';
 
